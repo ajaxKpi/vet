06ab3644b49dbf5c675cff3d73e38a5ef0d0f5f0
Ivan <WorkWin7@mail.com>
2016-01-17 23:22:19 +0200

    tanja modification

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 // eye blinking
 TO_interval =1000;
-Slider_TO =7000;
+Slider_TO =10000;
 
 // preloader time
 $(".loader").delay(2000).fadeOut("slow");
@@ -174,17 +174,17 @@ $(window).resize(function(){
 });
 
 //set color and remove cover of svg img
-$(".A_left").animated("fadeInLeft", "fadeOutDown");
-$(".row1").animated("fadeInUp", "fadeOutDown");
-$(".A_rigth").animated("fadeInRight", "fadeOutDown");
+//$(".A_left").animated("fadeInLeft", "fadeOutDown");
+$(".container.allservices").animated("fadeInUp", "fadeOutDown");
+//$(".A_rigth").animated("fadeInRight", "fadeOutDown");
 //rigth appear when scroll
 $("#doctor").animated("fadeInRight", "fadeOutDown");
 $(".dog_svg").animated("fadeInDownBig", "fadeOutDown");
 //$(".icon").animated("fadeInRight","fadeOutDown");
 
 
-var items = ["img/firstpage_background_hd.jpg", "img/1.jpg","img/21.jpg", "img/2.jpg","img/31.jpg", "img/3.jpg",
-	"img/41.jpg", "img/4.jpg","img/51.jpg","img/5.jpg","img/61.jpg","img/6.jpg"]
+var items = ["img/firstpage_background_hd.jpg", "img/1.jpg","img/2.jpg", "img/3.jpg",
+	 "img/4.jpg","img/5.jpg","img/6.jpg"]
 
 
 var currentIndex = 0, itemAmt = items.length, prevIndex =0;
